827fd62cfc8b4d48ded9294c28f8062bd9ce746e
Rahul Dey <rahuldey832000@gmail.com>
2024-03-07 23:15:34 +0530

    Add client side and server side validation with error message

--- VillaBooking.Domain/Entities/Villa.cs
+++ VillaBooking.Domain/Entities/Villa.cs
@@ -5,15 +5,22 @@ namespace VillaBooking.Domain.Entities;
 public class Villa
 {
     [Key]
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public string? Description { get; set; }
+    public int Id { get; init; }
+    [MinLength(5)]
+    [MaxLength(50)]
+    public required string Name { get; init; }
+    [MaxLength(5000)]
+    public string? Description { get; init; }
     [Display(Name = "Price Per Night")]
-    public double Price { get; set; }
-    public int Sqft { get; set; }
-    public int Occupancy { get; set; }
+    [Range(10,10000)]
+    public required double Price { get; init; }
+    [Range(100, 5000)]
+    public required int Sqft { get; init; }
+    [Range(1,10)]
+    public required int Occupancy { get; init; }
     [Display(Name = "Image Url")]
-    public string? ImageUrl { get; set; }
-    public DateTime CreatedDateTime { get; set; }
-    public DateTime UpdatedDateTime { get; set; }
+    [Url]
+    public string? ImageUrl { get; init; }
+    public DateTime CreatedDateTime { get; init; }
+    public DateTime UpdatedDateTime { get; init; }
 }
--- VillaBooking.Web/Controllers/VillaController.cs
+++ VillaBooking.Web/Controllers/VillaController.cs
@@ -1,21 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
+using VillaBooking.Domain.Entities;
 using VillaBooking.Infrastructure.Data;
 
 namespace VillaBooking.Web.Controllers;
 
+/// <summary>
+/// Represents the villa controller for views.
+/// </summary>
+/// <param name="context">The database context.</param>
 public class VillaController(AppDbContext context) : Controller
 {
+    /// <summary>
+    /// Represents the application database context.
+    /// </summary>
     private readonly AppDbContext _context = context;
 
-    // GET
+    /// <summary>
+    /// View list of all villa.
+    /// </summary>
+    /// <returns>Return villa index page with list of all villa.</returns>
+    [HttpGet]
     public IActionResult Index()
     {
         var villas = _context.Villas.ToList();
         return View(villas);
     }
 
+    /// <summary>
+    /// Create villa page.
+    /// </summary>
+    /// <returns>Return the create villa view page.</returns>
+    [HttpGet]
     public IActionResult Create()
     {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Villa model)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Villas.Add(model);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Villa");
+        }
+
         return View();
     }
 }
